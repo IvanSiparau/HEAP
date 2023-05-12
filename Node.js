455208969faff15d7e6fc53890dd867e79797b04
Vera Gretskaya <vigretskaya@edu.hse.ru>
2023-05-12 03:19:49 +0300

    улучшил появления новой вершины

--- Node.js
+++ Node.js
@@ -1,79 +1,92 @@
 class Node {
-  constructor(key, priority) {
-    this.key = key;
-    this.priority = priority;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-  }
+    constructor(key, priority) {
+        this.key = key;
+        this.priority = priority;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
+}
+
+function find(nodess, findNode) {
+    if (nodess != null) {
+        if (nodess.key === findNode.key && nodess.priority === findNode.priority) {
+            console.log(nodess);
+            return nodess;
+        }
+        find(nodess.left, findNode);
+        find(nodess.right, findNode);
+    }
 }
 
 function insertNode(lastNode, node) {
-  if (lastNode == null) {
-    node.loc = "root";
-    node.left = null;
-    node.right = null;
-    node.height = 1;
-    return node;
-  }
-  while((lastNode.parent != null) && (node.priority <= lastNode.priority)) {
-    drawLastAddedNoode(lastNode);
-    lastNode = lastNode.parent;
-  }
-  if ((lastNode.parent == null) && ((node.priority <= lastNode.priority))) {
-    node.loc = "root";
-    node.parent = null;
-    node.left = lastNode;
-    node.left.parent = node;
-    node.left.loc = "left";
-    node.height = getHeigth(lastNode) + 1;
-    mode = newAddedNodeBeginRootOfHeap;
-    return node;
-  } else  {
-    if (lastNode.right != null) {
-      lastNode.right.parent = node;
-      node.left = lastNode.right;
+    if (lastNode == null) {
+        node.loc = "root";
+        node.left = null;
+        node.right = null;
+        node.height = 1;
+        return node;
     }
-    lastNode.right = node;
-    node.parent = lastNode;
-    node.loc = "right";
-    if (node.left != null) {
-      node.left.loc = "left";
+    while ((lastNode.parent != null) && (node.priority <= lastNode.priority)) {
+        drawFirstAddedNode(lastNode);
+        lastNode = lastNode.parent;
     }
-    updateHeigth(node);
-    mode = newAddedNodeBeginTheRigthSonOfNode;
-    return node;
-  }
+    if ((lastNode.parent == null) && ((node.priority <= lastNode.priority))) {
+        node.loc = "root";
+        node.parent = null;
+        node.left = lastNode;
+        node.left.parent = node;
+        node.left.loc = "left";
+        node.height = getHeigth(lastNode) + 1;
+        mode = newAddedNodeBeginRootOfHeap;
+        return node;
+    } else {
+        if (lastNode.right != null) {
+            lastNode.right.parent = node;
+            node.left = lastNode.right;
+        }
+        lastNode.right = node;
+        node.parent = lastNode;
+        node.loc = "right";
+        if (node.left != null) {
+            node.left.loc = "left";
+        }
+        updateHeigth(node);
+        mode = newAddedNodeBeginTheRigthSonOfNode;
+        return node;
+    }
 }
 
 
 function buildCartesianTree(nodes) {
-  let root = null;
-  let lastNode = null;
-  let isFirst = true;
-  let newOurHeap = null;
-  for (let i = 0; i < nodes.length; i++) { 
-    const [key, priority] = nodes[i];
-    const node = new Node(key, priority);
-    if (!isFirst) {
-      drawNewAddedNode(node);
+    let root = null;
+    let lastNode = null;
+    let isFirst = true;
+    let newOurHeap = null;
+    for (let i = 0; i < nodes.length; i++) {
+        const [key, priority] = nodes[i];
+        const node = new Node(key, priority);
+/*        if (!isFirst) {
+            drawNewAddedNode(node);
+        }*/
+        root = insertNode(root, node);
+        newOurHeap = update(root, node);
+        if (isFirst) {
+            drawFirstNode(newOurHeap);
+            isFirst = false;
+            drawFirstAddedNode(node);
+        } else {
+            drawUpdate(newOurHeap, node);
+            let NewNode = find(newOurHeap, node);
+            //console.log(NewNode);
+            count++;
+            if (mode == newAddedNodeBeginRootOfHeap) {
+                drawNewLineBetweenOurHeapAndNewNode(root, lastNode, mode);
+            } else if (mode == newAddedNodeBeginTheRigthSonOfNode) {
+                drawNewLineBetweenOurHeapAndNewNode(node, lastNode, mode);
+            }
+        }
+        lastNode = node;
     }
-    root = insertNode(root, node);
-    newOurHeap = update(root, node);
-    if (isFirst) {
-      drawFirstNode(newOurHeap);
-      isFirst = false;
-    } else {
-      drawUpdate(newOurHeap, node);
-      count++;
-      if (mode == newAddedNodeBeginRootOfHeap) {
-        drawNewLineBetweenOurHeapAndNewNode(root, lastNode, mode);
-      } else if (mode == newAddedNodeBeginTheRigthSonOfNode) {
-        drawNewLineBetweenOurHeapAndNewNode(node, lastNode, mode);
-      }
-    }
-    lastNode = node;
-    drawFirstByPass(node);
-  }
-  return root;
+    return root;
 }
--- draw.js
+++ draw.js
@@ -1,127 +1,140 @@
 function drawAllHeap(node) {
-  if (node != null) {
-    if (node.parent != null) {
-      const line = d3.line()
-        .x(d => d.x)
-        .y(d => d.y);
-      const data1 = [
-        { x: node.x, y: node.y },
-        { x: node.parent.x, y: node.parent.y }
+    if (node != null) {
+        if (node.parent != null) {
+            const line = d3.line()
+                .x(d => d.x)
+                .y(d => d.y);
+            const data1 = [
+                {x: node.x, y: node.y},
+                {x: node.parent.x, y: node.parent.y}
             ];
-      svg.append("path")
-        .datum(data1)
-        .attr("d", line)
-        .attr("stroke", "black")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
+            svg.append("path")
+                .datum(data1)
+                .attr("d", line)
+                .attr("stroke", "black")
+                .attr("stroke-width", 2)
+                .attr("fill", "none");
+        }
+        svg.append("circle")
+            .attr("cx", node.x)
+            .attr("cy", node.y)
+            .attr('r', radius)
+            .attr('fill', 'red');
+        svg.append('text')
+            .attr('x', node.x)
+            .attr('y', node.y)
+            .attr('fill', 'black')
+            .text(node.x);
+        drawAllHeap(node.left);
+        drawAllHeap(node.right);
     }
-    svg.append("circle")
-      .attr("cx", node.x)
-      .attr("cy", node.y)
-      .attr('r', radius)
-      .attr('fill', 'red');
-    svg.append('text')
-      .attr('x', node.x)
-      .attr('y', node.y)
-      .attr('fill', 'black')
-      .text(node.x);
-    drawAllHeap(node.left);
-    drawAllHeap(node.right);
-  }
 }
 
-function getNameOfClassNode (node) {
-  if (node != null) {
-  return "NodeWithKey" + String(node.key) + "AndPrioriy" + String(node.priority);}
+function getNameOfClassNode(node) {
+    if (node != null) {
+        return "NodeWithKey" + String(node.key) + "AndPrioriy" + String(node.priority);
+    }
 }
 
 function getNameOfClassBetweenTwoNode(firstNode, secondNode) {
-  return 'LineBetween' + getNameOfClassNode(firstNode) + getNameOfClassNode(secondNode);
+    return 'LineBetween' + getNameOfClassNode(firstNode) + getNameOfClassNode(secondNode);
 }
 
-function drawFirstNode (node) {
-  const circle = svg.append("circle")
-    .attr("cx", node.x)
-    .attr("cy", node.y)
-    .attr('class', getNameOfClassNode(node))
-    .attr('r', 0)
-    .attr("fill", "white");
-  circle.transition()
-    .duration(time)
-    .attr("r", radius)
-    .delay(time * count)
-    .style("fill", '#960000');
-  count++;
+function drawFirstNode(node) {
+    const circle = svg.append("circle")
+        .attr("cx", node.x)
+        .attr("cy", node.y)
+        .attr('class', getNameOfClassNode(node))
+        .attr('r', 0)
+        .attr("fill", "white");
+    circle.transition()
+        .duration(time)
+        .attr("r", radius)
+        .delay(time * count)
+        .style("fill", '#960000');
+    count++;
 }
 
 
 function drawNewAddedNode(node, lastNode) {
-  const circle = svg.append("circle")
-    .attr("cx", 50)
-    .attr("cy", 400)
-    .attr('class', getNameOfClassNode(node))
-    .attr('r', 0)
-    .attr("fill", "white");
-  circle.transition()
-    .duration(time)
-    .attr("r", radius)
-    .delay(time * count)
-    .style("fill", '#960000');
-  count++;
+    const circle = svg.append("circle")
+        .attr("cx", 50)
+        .attr("cy", 400)
+        .attr('class', getNameOfClassNode(node))
+        .attr('r', 0)
+        .attr("fill", "white");
+    circle.transition()
+        .duration(time)
+        .attr("r", radius)
+        .delay(time * count)
+        .style("fill", '#960000');
+    count++;
 }
 
-function drawUpdate(root, node){
-  if (root != null) {
-    const circle = d3.select("." + getNameOfClassNode(root));
-    circle.transition()
-      .duration(time)
-      .attr("cx", root.x)
-      .attr('cy', root.y)
-      .delay(time * count);
-    if (root.left != null) {
-      d3.select("." + getNameOfClassBetweenTwoNode(root, root.left))
-        .transition()
-        .duration(time)
-        .attr('x1', root.x)
-        .attr('y1', root.y)
-        .attr('x2', root.left.x)
-        .attr('y2', root.left.y)
-        .delay(time*count);
-    } if (root.right != null) {
-        d3.select("." + getNameOfClassBetweenTwoNode(root, root.right))
-          .transition()
-          .duration(time)
-          .attr('x1', root.x)
-          .attr('y1', root.y)
-          .attr('x2', root.right.x)
-          .attr('y2', root.right.y)
-          .delay(time * count);
+function drawUpdate(root, node) {
+    if (root != null) {
+        const circle = d3.select("." + getNameOfClassNode(root));
+        circle.transition()
+            .duration(time)
+            .attr("cx", root.x)
+            .attr('cy', root.y)
+            .delay(time * count);
+        if (root.left != null) {
+            d3.select("." + getNameOfClassBetweenTwoNode(root, root.left))
+                .transition()
+                .duration(time)
+                .attr('x1', root.x)
+                .attr('y1', root.y)
+                .attr('x2', root.left.x)
+                .attr('y2', root.left.y)
+                .delay(time * count);
         }
-    if ((root.key == node.key && root.priority == node.priority)) {
-      const addedNode = d3.select('.theLastAddedNode');
-      addedNode.transition()
-        .duration(time)
-        .attr('cx', root.x)
-        .attr('cy', root.y)
-        .delay(time * count);
-      if (root.parent != null && root.left != null) {
-        d3.select("." + getNameOfClassBetweenTwoNode(root.parent, root.left))
-          .transition()
-          .duration(time)
-          .attr('x1', root.parent.x)
-          .attr('y1', root.parent.y)
-          .attr('x2', root.left.x)
-          .attr('y2', root.left.y)
-          .delay(time * count);
-      }
+        if (root.right != null) {
+            d3.select("." + getNameOfClassBetweenTwoNode(root, root.right))
+                .transition()
+                .duration(time)
+                .attr('x1', root.x)
+                .attr('y1', root.y)
+                .attr('x2', root.right.x)
+                .attr('y2', root.right.y)
+                .delay(time * count);
+        }
+        if ((root.key == node.key && root.priority == node.priority)) {
+            const circle = svg.append("circle")
+                .attr("cx", root.x)
+                .attr("cy", root.y)
+                .attr('class', getNameOfClassNode(node))
+                .attr('r', 0)
+                .attr("fill", "none");
+            circle.transition()
+                .duration(time)
+                .attr("r", radius)
+                .delay(time * count)
+                .style("fill", '#960000');
+            const addedNode = d3.select('.theLastAddedNode');
+            addedNode.transition()
+                .duration(time)
+                .attr('cx', root.x)
+                .attr('cy', root.y)
+                .delay(time * count);
+            if (root.parent != null && root.left != null) {
+                d3.select("." + getNameOfClassBetweenTwoNode(root.parent, root.left))
+                    .transition()
+                    .duration(time)
+                    .attr('x1', root.parent.x)
+                    .attr('y1', root.parent.y)
+                    .attr('x2', root.left.x)
+                    .attr('y2', root.left.y)
+                    .delay(time * count);
+            }
+        }
+        drawUpdate(root.left, node);
+        drawUpdate(root.right, node);
     }
-    drawUpdate(root.left, node);
-    drawUpdate(root.right, node);
-  }
 }
 
 function drawNewLineBetweenOurHeapAndNewNode(node, lastNode, modes) {
-  if (modes == newAddedNodeBeginRootOfHeap) {
+    if (modes == newAddedNodeBeginRootOfHeap) {
         var x1 = node.x, y1 = node.y;
         var x2 = node.left.x, y2 = node.left.y;
         var line = svg.append("line")
@@ -138,90 +151,69 @@ function drawNewLineBetweenOurHeapAndNewNode(node, lastNode, modes) {
             .delay(time * count)
             .attr("x2", x2)
             .attr("y2", y2);
-  } else if (modes == newAddedNodeBeginTheRigthSonOfNode) {
-     var x2 = node.x, y2 = node.y;
-     var x1 = node.parent.x, y1 = node.parent.y;
-     var lineBetweenParentOfAddedNodeAndNode = svg.append("line")
-       .attr("x1", x1)
-       .attr("y1", y1)
-       .attr("x2", x1)
-       .attr("y2", y1)
-       .attr('class', getNameOfClassBetweenTwoNode(node.parent, node))
-       .attr("stroke", "#960000")
-       .attr("stroke-opacity", 0.5)
-       .attr("stroke-width", 2);
-      lineBetweenParentOfAddedNodeAndNode.transition()
-       .duration(time)
-       .delay(time * count)
-       .attr("x2", x2)
-       .attr("y2", y2);
-      if (node.left != null) {
-        x1 = node.x, y1 = node.y, x2 = node.left.x, y2 = node.left.y;
-        var lineBetweenAddedNodeAndLeftSon = svg.append('line')
-          .attr("x1", x1)
-          .attr("y1", y1)
-          .attr("x2", x1)
-          .attr("y2", y1)
-          .attr('class', getNameOfClassBetweenTwoNode(node, node.left))
-          .attr("stroke", "#960000")
-          .attr("stroke-opacity", 0.5)
-          .attr("stroke-width", 2);
-        lineBetweenAddedNodeAndLeftSon.transition()
-          .duration(time)
-          .delay(time * count)
-          .attr("x2", x2)
-          .attr("y2", y2)
-        const removeLine = d3.select("." + getNameOfClassBetweenTwoNode(node.parent, node.left));
-        removeLine.transition()
-          .duration(time)
-          .attr("x2", node.parent.x)
-          .attr("y2", node.parent.y)
-          .remove()
-          .delay(count * time);
-      }
-  }
-  count++;
+    } else if (modes === newAddedNodeBeginTheRigthSonOfNode) {
+        var x2 = node.x, y2 = node.y;
+        var x1 = node.parent.x, y1 = node.parent.y;
+        var lineBetweenParentOfAddedNodeAndNode = svg.append("line")
+            .attr("x1", x1)
+            .attr("y1", y1)
+            .attr("x2", x1)
+            .attr("y2", y1)
+            .attr('class', getNameOfClassBetweenTwoNode(node.parent, node))
+            .attr("stroke", "#960000")
+            .attr("stroke-opacity", 0.5)
+            .attr("stroke-width", 2);
+        lineBetweenParentOfAddedNodeAndNode.transition()
+            .duration(time)
+            .delay(time * count)
+            .attr("x2", x2)
+            .attr("y2", y2);
+        if (node.left != null) {
+            x1 = node.x, y1 = node.y, x2 = node.left.x, y2 = node.left.y;
+            var lineBetweenAddedNodeAndLeftSon = svg.append('line')
+                .attr("x1", x1)
+                .attr("y1", y1)
+                .attr("x2", x1)
+                .attr("y2", y1)
+                .attr('class', getNameOfClassBetweenTwoNode(node, node.left))
+                .attr("stroke", "#960000")
+                .attr("stroke-opacity", 0.5)
+                .attr("stroke-width", 2);
+            lineBetweenAddedNodeAndLeftSon.transition()
+                .duration(time)
+                .delay(time * count)
+                .attr("x2", x2)
+                .attr("y2", y2)
+            const removeLine = d3.select("." + getNameOfClassBetweenTwoNode(node.parent, node.left));
+            removeLine.transition()
+                .duration(time)
+                .attr("x2", node.parent.x)
+                .attr("y2", node.parent.y)
+                .remove()
+                .delay(count * time);
+        }
+    }
+    count++;
 }
 
-function drawFirstByPass(nodesss) {
-var circle_ = svg.append("circle")
-  .attr("cx", nodesss.x)
-  .attr("cy", nodesss.y)
-  .attr("r", radius + 1)
-  .attr("fill", "none")
-  .attr("stroke", "blue")
-  .attr("class", "pass")
-  .attr("stroke-width", 0);
-  circle_.transition()
-  .duration(time)
-  .attr("stroke-width", 2)
-  .delay(time * count);
-  count++;
-    
-}
 function getCircle(node) {
-  var circle = svg.select('.' + getNameOfClassNode(node));
-  var x = circle.attr('cx');
-  var y = circle.attr('cy');
-  return [x, y];
+    var circle = svg.select('.' + getNameOfClassNode(node));
+    var x = circle.attr('cx');
+    var y = circle.attr('cy');
+    return [x, y];
 }
 
-function drawLastAddedNoode(nodes) {
-  var circless = svg.select('.pass');
-  circless.transition()
-    .duration(time)
-    .attr('cx' ,function(){
-          var circle = svg.select('.' + getNameOfClassNode(nodes));
-          var x = circle.attr('cx');
-          var y = circle.attr('cy');
-      return x;
-    })
-    .attr('cy', function() {
-                var circle = svg.select('.' + getNameOfClassNode(nodes));
-                var x = circle.attr('cx');
-                var y = circle.attr('cy');
-        return y;
-    })
-    .delay(time * (count));
-  count++;
+function drawFirstAddedNode(node) {
+    var newCircle = svg.append('circle')
+        .attr('cx', getCircle(node)[0])
+        .attr('cy', getCircle(node)[1])
+        .attr('r', radius + 1)
+        .attr('fill', 'none')
+        .attr('stroke', 'blue')
+        .attr('stroke-width', 0);
+    newCircle.transition()
+        .duration(time)
+        .attr('stroke-width', 2)
+        .delay(time * count);
+    count++;
 }
